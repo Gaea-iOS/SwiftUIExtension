b195a262d56fd23ca3358c1f45fc4fb3690f3d30
Jerry Wang <445242970@qq.com>
2023-05-30 10:47:45 +0800

    Add  PositionObservingView

---
+++ Sources/SwiftUIExtension/View/PositionObservingView/OffsetObservingScrollView.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by 王小涛 on 2023/5/30.
+//
+
+import SwiftUI
+
+/// Specialized scroll view that observes its content offset (scroll position)
+/// and assigns it to the specified Binding.
+public struct OffsetObservingScrollView<Content: View>: View {
+    public var axes: Axis.Set = [.vertical]
+    public var showsIndicators = true
+    @Binding public var offset: CGPoint
+    @ViewBuilder public var content: () -> Content
+
+    private let coordinateSpaceName = UUID()
+
+    public var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            PositionObservingView(
+                coordinateSpace: .named(coordinateSpaceName),
+                position: Binding(
+                    get: { offset },
+                    set: { newOffset in
+                        offset = CGPoint(
+                            x: -newOffset.x,
+                            y: -newOffset.y
+                        )
+                    }
+                ),
+                content: content
+            )
+        }
+        .coordinateSpace(name: coordinateSpaceName)
+    }
+}
---
+++ Sources/SwiftUIExtension/View/PositionObservingView/PositionObservingView.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  
+//
+//  Created by 王小涛 on 2023/5/30.
+//
+
+import SwiftUI
+
+struct PositionObservingView<Content: View>: View {
+    var coordinateSpace: CoordinateSpace
+    @Binding var position: CGPoint
+    @ViewBuilder var content: () -> Content
+
+    var body: some View {
+        content()
+            .background(GeometryReader { geometry in
+                Color.clear.preference(
+                    key: PreferenceKey.self,
+                    value: geometry.frame(in: coordinateSpace).origin
+                )
+            })
+            .onPreferenceChange(PreferenceKey.self) { position in
+                self.position = position
+            }
+    }
+}
+
+private extension PositionObservingView {
+    enum PreferenceKey: SwiftUI.PreferenceKey {
+        static var defaultValue: CGPoint { .zero }
+
+        static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {
+            // No-op
+        }
+    }
+}
