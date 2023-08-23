6b06492e4b32641e1b38176787a8a235f973edcf
Jerry Wang <445242970@qq.com>
2023-08-23 22:56:01 +0800

    Update

--- Sources/SwiftUIExtension/CalendarX/CalendarX+Day.swift
+++ Sources/SwiftUIExtension/CalendarX/CalendarX+Day.swift
@@ -87,16 +87,6 @@ extension CalendarX {
     }
 }
 
-public extension CalendarX.Day {
-    var month: CalendarX.Month {
-        .init(year: year, month: month)
-    }
-    
-    var week: CalendarX.Week {
-        .init(year: year, month: month, weekOfMonth: weekOfMonth)
-    }
-}
-
 //extension CalendarX.Day: Comparable {
 //    public static func < (lhs: Self, rhs: Self) -> Bool {
 //        lhs.year < rhs.year
