c7ab0230b042be62dc811d18e2a5960dfd552189
Jerry Wang <445242970@qq.com>
2023-08-23 21:59:44 +0800

    Fix bugs

--- Sources/SwiftUIExtension/CalendarX/CalendarX+Week.swift
+++ Sources/SwiftUIExtension/CalendarX/CalendarX+Week.swift
@@ -46,6 +46,14 @@ private extension CalendarX.Week {
     }
 }
 
+extension CalendarX.Week: Comparable {
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        lhs.year < rhs.year
+            || (lhs.year == rhs.year && lhs.month < rhs.month)
+            || (lhs.year == rhs.year && lhs.month == rhs.month && lhs.weekOfMonth < rhs.weekOfMonth)
+    }
+}
+
 public extension [CalendarX.Week] {
     func removingDuplicates() -> Self {
         var newWeeks: Self = []
