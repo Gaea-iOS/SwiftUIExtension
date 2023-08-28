78764f810dace0b13bd491ee782fc352f57378c1
Jerry Wang <445242970@qq.com>
2023-08-28 23:14:29 +0800

    Add ShareSheetView

---
+++ Sources/SwiftUIExtension/View/File.swift
@@ -0,0 +1,56 @@
+//
+//  File.swift
+//  
+//
+//  Created by 王小涛 on 2023/8/28.
+//
+
+import SwiftUI
+
+public struct ShareSheetView: UIViewControllerRepresentable {
+    public typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
+    public let activityItems: [Any]
+    public let applicationActivities: [UIActivity]? = nil
+    public let excludedActivityTypes: [UIActivity.ActivityType]? = {
+        var types: [UIActivity.ActivityType] = [
+            .print,
+            .assignToContact,
+            .saveToCameraRoll,
+            .addToReadingList,
+            .postToFlickr,
+            .postToVimeo,
+            .airDrop,
+            .openInIBooks,
+            .markupAsPDF
+        ]
+        
+        
+        if #available(iOS 15.4, *) {
+            types.append(.sharePlay)
+        }
+        
+        if #available(iOS 16.0, *) {
+            types.append(
+                contentsOf: [.collaborationInviteWithLink, .collaborationCopyLink]
+            )
+        }
+        
+        return types
+    }()
+
+    public let callback: Callback? = nil
+
+    public func makeUIViewController(context _: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: applicationActivities
+        )
+        controller.excludedActivityTypes = excludedActivityTypes
+        controller.completionWithItemsHandler = callback
+        return controller
+    }
+
+    public func updateUIViewController(_: UIActivityViewController, context _: Context) {
+        // nothing to do here
+    }
+}
