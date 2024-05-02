49408e6def78d21b032d554423d01d57b25c0d73
Jerry Wang <445242970@qq.com>
2024-05-02 22:02:16 +0800

    Update FeedbackButton.swift

--- Sources/SwiftUIExtension/View/FeedbackButton.swift
+++ Sources/SwiftUIExtension/View/FeedbackButton.swift
@@ -5,32 +5,32 @@
 //  Created by 王小涛 on 2023/9/3.
 //
 
-import SwiftUI
-
-public struct FeedbackButton<Label>: View where Label: View {
-    let feedback: FeedbackStyle
-    let action: () -> Void
-
-    @ViewBuilder let label: () -> Label
-
-    public init(
-        feedback: FeedbackStyle = .selection,
-        action: @escaping () -> Void,
-        @ViewBuilder label: @escaping () -> Label
-    ) {
-        self.feedback = feedback
-        self.action = action
-        self.label = label
-    }
-
-    public var body: some View {
-        Button(
-            action: {
-                action()
-            },
-            label: label
-        )
-        .buttonStyle(ScaleFeedbackButtonStyle())
-        .feedbackOnTapGesture(feedback: .light)
-    }
-}
+//import SwiftUI
+//
+//public struct FeedbackButton<Label>: View where Label: View {
+//    let feedback: FeedbackStyle
+//    let action: () -> Void
+//
+//    @ViewBuilder let label: () -> Label
+//
+//    public init(
+//        feedback: FeedbackStyle = .selection,
+//        action: @escaping () -> Void,
+//        @ViewBuilder label: @escaping () -> Label
+//    ) {
+//        self.feedback = feedback
+//        self.action = action
+//        self.label = label
+//    }
+//
+//    public var body: some View {
+//        Button(
+//            action: {
+//                action()
+//            },
+//            label: label
+//        )
+//        .buttonStyle(ScaleFeedbackButtonStyle())
+//        .feedbackOnTapGesture(feedback: .light)
+//    }
+//}
