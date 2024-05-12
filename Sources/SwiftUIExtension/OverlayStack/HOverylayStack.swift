cd19160128c70e6c41081cb88057d3e662c36ea6
Jerry Wang <445242970@qq.com>
2024-05-12 16:30:29 +0800

    Update.

---
+++ Sources/SwiftUIExtension/OverlayStack/HOverylayStack.swift
@@ -0,0 +1,65 @@
+//
+//  File.swift
+//  
+//
+//  Created by 王小涛 on 2024/5/12.
+//
+
+import Foundation
+import SwiftUI
+
+public struct HOverylayStack: Layout {
+    let overlaying: CGFloat
+    
+    public init(overlaying: CGFloat) {
+        self.overlaying = overlaying
+    }
+
+    public func sizeThatFits(proposal _: ProposedViewSize, subviews: Subviews, cache _: inout ()) -> CGSize {
+        // Calculate and return the size of the layout container.
+        let subviewSizes = subviews.map { proxy in
+            proxy.sizeThatFits(.unspecified)
+        }
+
+        let combinedWidth = subviewSizes.map(\.width).reduce(0, +)
+        let combineHeight = subviewSizes.map(\.height).max() ?? 0
+        let realWidth = combinedWidth - overlaying * CGFloat(subviewSizes.count - 1)
+        return .init(width: realWidth, height: combineHeight)
+    }
+
+    public func placeSubviews(in bounds: CGRect, proposal _: ProposedViewSize, subviews: Subviews, cache _: inout ()) {
+        // Tell each subview where to appear.
+        let subviewSizes = subviews.map { proxy in
+            proxy.sizeThatFits(.unspecified)
+        }
+
+        var x = bounds.minX
+        var y = 0.0
+        for index in subviews.indices {
+            let subviewSize = subviewSizes[index]
+            let sizeProposal = ProposedViewSize(
+                width: subviewSize.width,
+                height: subviewSize.height
+            )
+
+            y = bounds.minY + (bounds.height - subviewSize.height) / 2
+            subviews[index]
+                .place(
+                    at: .init(x: x, y: y),
+                    anchor: .topLeading,
+                    proposal: sizeProposal
+                )
+
+            x += subviewSize.width - overlaying
+        }
+    }
+}
+
+#Preview {
+    HOverylayStack(overlaying: 10) {
+        Color.red.frame(width: 100, height: 100)
+        Color.yellow.frame(width: 30, height: 30)
+        Color.blue.frame(width: 40, height: 40)
+    }
+    .background(Color.black)
+}
---
+++ Sources/SwiftUIExtension/OverlayStack/VOverylayStack.swift
@@ -0,0 +1,65 @@
+//
+//  File.swift
+//  
+//
+//  Created by 王小涛 on 2024/5/12.
+//
+
+import Foundation
+import SwiftUI
+
+public struct VOverylayStack: Layout {
+    let overlaying: CGFloat
+    
+    public init(overlaying: CGFloat) {
+        self.overlaying = overlaying
+    }
+
+    public func sizeThatFits(proposal _: ProposedViewSize, subviews: Subviews, cache _: inout ()) -> CGSize {
+        // Calculate and return the size of the layout container.
+        let subviewSizes = subviews.map { proxy in
+            proxy.sizeThatFits(.unspecified)
+        }
+
+        let combinedHeight = subviewSizes.map(\.height).reduce(0, +)
+        let maxWidth = subviewSizes.map(\.width).max() ?? 0
+        let realHeight = combinedHeight - overlaying * CGFloat(subviewSizes.count - 1)
+        return .init(width: maxWidth, height: realHeight)
+    }
+
+    public func placeSubviews(in bounds: CGRect, proposal _: ProposedViewSize, subviews: Subviews, cache _: inout ()) {
+        // Tell each subview where to appear.
+        let subviewSizes = subviews.map { proxy in
+            proxy.sizeThatFits(.unspecified)
+        }
+
+        var x = 0.0
+        var y = bounds.minY
+        for index in subviews.indices {
+            let subviewSize = subviewSizes[index]
+            let sizeProposal = ProposedViewSize(
+                width: subviewSize.width,
+                height: subviewSize.height
+            )
+
+            x = bounds.minX + (bounds.width - subviewSize.width) / 2
+            subviews[index]
+                .place(
+                    at: .init(x: x, y: y),
+                    anchor: .topLeading,
+                    proposal: sizeProposal
+                )
+
+            y += subviewSize.height - overlaying
+        }
+    }
+}
+
+#Preview {
+    VOverylayStack(overlaying: 10) {
+        Color.red.frame(width: 100, height: 100)
+        Color.yellow.frame(width: 30, height: 30)
+        Color.blue.frame(width: 40, height: 40)
+    }
+    .background(Color.black)
+}
