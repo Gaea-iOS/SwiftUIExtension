69f4921e946232cbf2cd9079c7b42d14706c6682
Jerry Wang <445242970@qq.com>
2025-12-17 10:46:17 +0800

    Add min deploy iOS version iOS17

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIExtension",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .watchOS(.v6)
     ],
     products: [
