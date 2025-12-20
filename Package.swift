c3e8e44edaa8d7e4b219b4def93a0da3ef2cc731
Jerry Wang <445242970@qq.com>
2025-12-20 12:01:30 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Gaea-iOS/MobileCore.git", branch: "main")
+        .package(
+            url: "https://github.com/Gaea-iOS/MobileCore.git",
+            revision: "d11e01b6dd6d4e3896d437881f5195fe6158581b"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
