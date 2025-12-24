48d41232d421997e9702d7cb1eb524ff0064f0f8
Jerry Wang <445242970@qq.com>
2025-12-24 22:37:44 +0800

    Update MobileCore

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
             url: "https://github.com/Gaea-iOS/MobileCore.git",
-            revision: "d11e01b6dd6d4e3896d437881f5195fe6158581b"
+            revision: "78b1ab7df391725caf0abc64b2f5796d9f805cee"
         )
     ],
     targets: [
